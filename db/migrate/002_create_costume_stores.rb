a3ef8540bf2557c842f55a19b56c7bc24d4c5d27
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-26 21:39:06 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,7 +2,12 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change 
     create_table :costume_stores do |s|
-      
+      s.string :name 
+      s.string :location
+      s.integer :costume_inventory 
+      s.integer :number_of_employees 
+      s.boolean :in_business
+      s.
     end 
   end 
 end 
