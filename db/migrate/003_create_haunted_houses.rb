08914b5d3952c9f44d3c15243deb6fad5c3b8cee
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-26 21:35:48 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,15 +2,5 @@
 
 class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change 
-    create_table :haunted_houses do |h|
-      h.string :name
-      h.string :location
-      h.string :theme
-      h.float :price
-      h.boolean :family_friendly
-      h.timestamp :opening_date
-      h.timestamp :closing_date
-      h.text :description
-    end 
   end 
 end 
