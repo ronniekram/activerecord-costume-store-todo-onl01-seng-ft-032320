70610ff47d62d0770a0238f632178e90581e0d66
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-26 21:35:29 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,12 +10,5 @@
 
 class CreateCostumes < ActiveRecord::Migration[5.1]
   def change 
-    create_table :costumes do |c|
-      c.string :name 
-      c.float :price
-      c.string :size 
-      c.string :image_url
-      c.timestamps
-    end
   end 
 end
