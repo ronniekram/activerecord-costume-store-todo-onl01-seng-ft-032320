30401b70ccfd6c75a1197b2e9548bd8a13b06466
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-26 21:37:58 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,8 @@
 # Create your costume_stores migration here
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change 
+    create_table :costume_stores do |s|
+      
+    end 
   end 
 end 
